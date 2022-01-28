4c2701d35dcbf7d955633f501fe4e6d11330d9aa
panjivj <panjijayasutra@gmail.com>
2022-01-28 22:05:52 +0700

    update

---
+++ sambac-jasmine/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="mx-auto content-center">
+		<!-- <img class="mx-auto" alt="Vue logo" src="./assets/logo.png" /> -->
+		<h3 class="bg-pink-400 mx-auto text-center">Hi</h3>
+		<div class="tes">
+			<div class="ya">hello</div>
+		</div>
+		<HelloWorldVue />
+	</div>
+</template>
+<script setup>
+import HelloWorldVue from "./components/HelloWorld.vue";
+</script>
+<style lang="scss">
+.tes {
+	text-align: center;
+	.ya {
+		color: green;
+	}
+}
+</style>
---
+++ sambac-jasmine/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<el-row class="mb-4">
+			<el-button>Default</el-button>
+			<el-button type="primary">Primary</el-button>
+			<el-button type="success">Success</el-button>
+			<el-button type="info">Info</el-button>
+			<el-button type="warning">Warning</el-button>
+			<el-button type="danger">Danger</el-button>
+			<el-button>中文</el-button>
+		</el-row>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+a {
+	color: #42b983;
+}
+:root {
+	--el-color-primary: #409eff;
+	--el-color-success: #67c23a;
+	--el-color-warning: #e6a23c;
+	--el-color-danger: #f56c6c;
+	--el-color-error: #f56c6c;
+	--el-color-info: #909399;
+}
+</style>
